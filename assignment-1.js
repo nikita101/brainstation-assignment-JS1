c1308da2f289ba59add2ee774e31fcea364576a6
nikita101 <n.odnoralov@gmil.com>
2018-04-05 09:24:24 -0500

    first commit

---
+++ assignment-1.js
@@ -0,0 +1,16 @@
+
+
+
+var numbers = [ ];
+
+for (var i=0; i<10; i++) {
+	numbers.push(i)}
+ 
+console.log(numbers);
+
+console.log(numbers[0])
+console.log(numbers[9])
+
+var car = {
+}
+car.colour = "blank"
